89901e256ab969ab8ec7ac0b8e14ce6c2f566360
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-04 15:13:46 +0300

    Navbar

---
+++ pairprogramming/src/components/WarningSign.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Alert from 'react-bootstrap/Alert'
+
+
+
+class Warnings extends React.Component{
+    
+    render(){
+        return(
+            <div>Hello</div>
+        );
+    }
+    
+}
+export default Warnings
